2aceea7a0a4147c1f0cccb050abbd8eaa2b3a2a6
dasunNimantha <dasunnimantha77@outlook.com>
2021-06-23 09:11:35 +0530

    Add project files.

---
+++ SSE/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace SSE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(25, 83);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(146, 27);
+            this.btnChoose.TabIndex = 0;
+            this.btnChoose.Text = "Choose Video File";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Enabled = false;
+            this.txtPath.Location = new System.Drawing.Point(194, 85);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(382, 22);
+            this.txtPath.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(232, 161);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 17);
+            this.lblStatus.TabIndex = 2;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Enabled = false;
+            this.btnEncrypt.Location = new System.Drawing.Point(208, 202);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(143, 43);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Encrypt Now";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 302);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnChoose);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Secure Stream Encryptor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnEncrypt;
+    }
+}
+
---
+++ SSE/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SSE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        string filePath = "";
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Videos|*.mp4;*.mkv;*.m4v";
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = ofd.FileName;
+                btnEncrypt.Enabled = true;
+                filePath = "\"" + ofd.FileName + "\"";
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string m3uout = path + "\\index.m3u8";
+            string output = "output\\fileSequence%d.ts";
+            Process process = Process.Start("cmd.exe",$@"/k cd {path} && mkdir output && ffmpeg.exe -i {filePath} -hls_time 9  -hls_playlist_type vod -hls_segment_filename {output} {m3uout} &&  del /Q output  && rmdir output");       
+        }
+    }
+}
