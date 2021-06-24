b34564037ddd840a77d7f3add0797e904b0ebe1b
dasunNimantha <dasunnimantha77@outlook.com>
2021-06-24 13:53:50 +0530

    finished basic functions

--- SSE/Form1.Designer.cs
+++ SSE/Form1.Designer.cs
@@ -39,13 +39,19 @@ namespace SSE
             this.lblPass = new System.Windows.Forms.Label();
             this.txtPass = new System.Windows.Forms.TextBox();
             this.checkNvidia = new System.Windows.Forms.CheckBox();
+            this.lblStatLable = new System.Windows.Forms.Label();
+            this.lblStat = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSessionName = new System.Windows.Forms.TextBox();
+            this.checkLow = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // btnChoose
             // 
-            this.btnChoose.Location = new System.Drawing.Point(25, 83);
+            this.btnChoose.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoose.Location = new System.Drawing.Point(24, 51);
             this.btnChoose.Name = "btnChoose";
-            this.btnChoose.Size = new System.Drawing.Size(146, 38);
+            this.btnChoose.Size = new System.Drawing.Size(146, 40);
             this.btnChoose.TabIndex = 0;
             this.btnChoose.Text = "Choose Video File";
             this.btnChoose.UseVisualStyleBackColor = true;
@@ -54,15 +60,16 @@ namespace SSE
             // txtPath
             // 
             this.txtPath.Enabled = false;
-            this.txtPath.Location = new System.Drawing.Point(194, 85);
+            this.txtPath.Font = new System.Drawing.Font("Segoe UI", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPath.Location = new System.Drawing.Point(193, 58);
             this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(382, 22);
+            this.txtPath.Size = new System.Drawing.Size(356, 26);
             this.txtPath.TabIndex = 1;
             // 
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(232, 161);
+            this.lblStatus.Location = new System.Drawing.Point(231, 129);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(0, 17);
             this.lblStatus.TabIndex = 2;
@@ -70,7 +77,8 @@ namespace SSE
             // btnEncrypt
             // 
             this.btnEncrypt.Enabled = false;
-            this.btnEncrypt.Location = new System.Drawing.Point(217, 407);
+            this.btnEncrypt.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.Location = new System.Drawing.Point(218, 500);
             this.btnEncrypt.Name = "btnEncrypt";
             this.btnEncrypt.Size = new System.Drawing.Size(143, 43);
             this.btnEncrypt.TabIndex = 3;
@@ -80,9 +88,10 @@ namespace SSE
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 161);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(24, 129);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 35);
+            this.button1.Size = new System.Drawing.Size(146, 40);
             this.button1.TabIndex = 4;
             this.button1.Text = "Output Directory";
             this.button1.UseVisualStyleBackColor = true;
@@ -90,44 +99,109 @@ namespace SSE
             // 
             // txtOutput
             // 
-            this.txtOutput.Location = new System.Drawing.Point(194, 167);
+            this.txtOutput.Enabled = false;
+            this.txtOutput.Font = new System.Drawing.Font("Segoe UI", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(193, 135);
             this.txtOutput.Name = "txtOutput";
-            this.txtOutput.Size = new System.Drawing.Size(382, 22);
+            this.txtOutput.Size = new System.Drawing.Size(356, 26);
             this.txtOutput.TabIndex = 5;
             // 
             // lblPass
             // 
             this.lblPass.BackColor = System.Drawing.Color.SpringGreen;
-            this.lblPass.Location = new System.Drawing.Point(25, 243);
+            this.lblPass.Font = new System.Drawing.Font("Segoe UI", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(24, 272);
             this.lblPass.Name = "lblPass";
-            this.lblPass.Size = new System.Drawing.Size(146, 37);
+            this.lblPass.Size = new System.Drawing.Size(146, 40);
             this.lblPass.TabIndex = 6;
             this.lblPass.Text = "Password";
             this.lblPass.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(194, 250);
+            this.txtPass.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(193, 281);
             this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(249, 22);
+            this.txtPass.Size = new System.Drawing.Size(249, 25);
             this.txtPass.TabIndex = 7;
             this.txtPass.UseSystemPasswordChar = true;
             // 
             // checkNvidia
             // 
-            this.checkNvidia.AutoSize = true;
-            this.checkNvidia.Location = new System.Drawing.Point(325, 341);
+            this.checkNvidia.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkNvidia.Location = new System.Drawing.Point(264, 354);
             this.checkNvidia.Name = "checkNvidia";
-            this.checkNvidia.Size = new System.Drawing.Size(274, 21);
+            this.checkNvidia.Size = new System.Drawing.Size(336, 21);
             this.checkNvidia.TabIndex = 8;
             this.checkNvidia.Text = "Enable Hardware Acceleration (Nvidia)";
             this.checkNvidia.UseVisualStyleBackColor = true;
+            this.checkNvidia.CheckedChanged += new System.EventHandler(this.checkNvidia_CheckedChanged);
+            // 
+            // lblStatLable
+            // 
+            this.lblStatLable.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblStatLable.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatLable.Location = new System.Drawing.Point(119, 425);
+            this.lblStatLable.Name = "lblStatLable";
+            this.lblStatLable.Size = new System.Drawing.Size(183, 39);
+            this.lblStatLable.TabIndex = 9;
+            this.lblStatLable.Text = "Encryption Status   :";
+            this.lblStatLable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblStat
+            // 
+            this.lblStat.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblStat.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStat.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblStat.Location = new System.Drawing.Point(282, 425);
+            this.lblStat.Name = "lblStat";
+            this.lblStat.Size = new System.Drawing.Size(207, 39);
+            this.lblStat.TabIndex = 10;
+            this.lblStat.Text = "IDLE";
+            this.lblStat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 40);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Session Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtSessionName
+            // 
+            this.txtSessionName.Font = new System.Drawing.Font("Segoe UI", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSessionName.Location = new System.Drawing.Point(193, 205);
+            this.txtSessionName.Name = "txtSessionName";
+            this.txtSessionName.Size = new System.Drawing.Size(356, 26);
+            this.txtSessionName.TabIndex = 12;
+            // 
+            // checkLow
+            // 
+            this.checkLow.AutoSize = true;
+            this.checkLow.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkLow.Location = new System.Drawing.Point(39, 354);
+            this.checkLow.Name = "checkLow";
+            this.checkLow.Size = new System.Drawing.Size(176, 23);
+            this.checkLow.TabIndex = 13;
+            this.checkLow.Text = "Low Performance Mode";
+            this.checkLow.UseVisualStyleBackColor = true;
+            this.checkLow.CheckedChanged += new System.EventHandler(this.checkLow_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(611, 494);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(573, 568);
+            this.Controls.Add(this.checkLow);
+            this.Controls.Add(this.txtSessionName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStat);
+            this.Controls.Add(this.lblStatLable);
             this.Controls.Add(this.checkNvidia);
             this.Controls.Add(this.txtPass);
             this.Controls.Add(this.lblPass);
@@ -138,6 +212,7 @@ namespace SSE
             this.Controls.Add(this.txtPath);
             this.Controls.Add(this.btnChoose);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Secure Stream Encryptor";
@@ -158,6 +233,11 @@ namespace SSE
         private System.Windows.Forms.Label lblPass;
         private System.Windows.Forms.TextBox txtPass;
         private System.Windows.Forms.CheckBox checkNvidia;
+        private System.Windows.Forms.Label lblStatLable;
+        private System.Windows.Forms.Label lblStat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSessionName;
+        private System.Windows.Forms.CheckBox checkLow;
     }
 }
 
--- SSE/Form1.cs
+++ SSE/Form1.cs
@@ -25,45 +25,55 @@ namespace SSE
         }
 
         OpenFileDialog ofd = new OpenFileDialog();
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
         public string filePath = "";
+        public string outputPath = "";
+        public string hash = null;
         public string userPasss = null;
        
 
         
         private void Form1_Load(object sender, EventArgs e)
-         {
-
+        {
+            SqlConnection sqlcon = new SqlConnection(Properties.Settings.Default.loginDBConnectionString);
+            string query = "Select * from mainDB where id=1";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            hash = dt.Rows[0][1].ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ofd.Filter = "Videos|*.mp4;*.mkv;*.m4v";
-
             if(ofd.ShowDialog() == DialogResult.OK)
             {
                 txtPath.Text = ofd.FileName;
-                btnEncrypt.Enabled = true;
                 filePath = "\"" + ofd.FileName + "\"";
+            }
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtOutput.Text = fbd.SelectedPath;
+                outputPath = fbd.SelectedPath;
+                btnEncrypt.Enabled = true;
             }
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            if (String.IsNullOrEmpty(txtPass.Text))
-            {
+            if (String.IsNullOrEmpty(txtSessionName.Text))
+            {  
+                MessageBox.Show("Session Name is required");
+
+            } else if(String.IsNullOrEmpty(txtPass.Text)){
                 MessageBox.Show("Password is required");
-                
-            } else
+            } else 
+
             {
 
-                SqlConnection sqlcon = new SqlConnection(Properties.Settings.Default.loginDBConnectionString);
-                string query = "Select * from mainDB where id=1";
-                SqlDataAdapter sda = new SqlDataAdapter(query,sqlcon);
-                DataTable dt = new DataTable();
-                sda.Fill(dt);
-                string hash = dt.Rows[0][1].ToString();
-                
-                
                 var checkpass = BCrypt.Net.BCrypt.Verify(txtPass.Text, hash );
                 if (!checkpass)
                 {
@@ -72,6 +82,10 @@ namespace SSE
                 } else
 
                 {
+                    btnEncrypt.Enabled = false;
+                    lblStat.ForeColor = Color.DarkOrange;
+                    lblStat.Text = "Encrpyting... ";
+                   
                     string path = Directory.GetCurrentDirectory();
                     string m3uout = "index.m3u8";
                     string output = "fileSequence%d.ts";
@@ -83,7 +97,17 @@ namespace SSE
                         startInfo.UseShellExecute = false;
                         startInfo.FileName = "cmd.exe";
                         startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                        startInfo.Arguments = $@"/k cd {path} && mkdir output && cd output && openssl rand 16 > enc.key && echo enc.key > enc.keyinfo && echo enc.key >> enc.keyinfo && ffmpeg.exe -i {filePath} -hls_time 100 -hls_key_info_file enc.keyinfo  -hls_playlist_type vod -hls_segment_filename {output} {m3uout} && exit";
+                        if (checkLow.Checked)
+                        {
+                            startInfo.Arguments = $@"/k cd {path} && mkdir output && cd output && openssl rand 16 > enc.key && echo enc.key > enc.keyinfo && echo enc.key >> enc.keyinfo && ffmpeg.exe -vcodec h264_qsv  -i {filePath} -hls_time 100 -hls_key_info_file enc.keyinfo  -hls_playlist_type vod -hls_segment_filename {output} {m3uout} && exit";
+                        } else if (checkNvidia.Checked)
+                        {
+                            startInfo.Arguments = $@"/k cd {path} && mkdir output && cd output && openssl rand 16 > enc.key && echo enc.key > enc.keyinfo && echo enc.key >> enc.keyinfo && ffmpeg.exe -hwaccel cuda  -i {filePath} -hls_time 100 -hls_key_info_file enc.keyinfo  -hls_playlist_type vod -hls_segment_filename {output} {m3uout} && exit";
+                        } else
+                        {
+                            startInfo.Arguments = $@"/k cd {path} && mkdir output && cd output && openssl rand 16 > enc.key && echo enc.key > enc.keyinfo && echo enc.key >> enc.keyinfo && ffmpeg.exe -i {filePath} -hls_time 100 -hls_key_info_file enc.keyinfo  -hls_playlist_type vod -hls_segment_filename {output} {m3uout} && exit";
+                        }
+                      
 
                         using (Process exeProcess = Process.Start(startInfo))
                         {
@@ -91,13 +115,15 @@ namespace SSE
 
                         }
 
+                        lblStat.ForeColor = Color.Blue;
+                        lblStat.Text = "Copying Files... ";
                         string startPath = $@"{path}\\output\\";
                         string destPath = $@"{path}\\info.zip";
 
                         using (ZipFile zip = new ZipFile())
                         {
 
-                            zip.Password = "123";
+                            zip.Password = "pass1";
                             zip.AddDirectory(startPath);
                             zip.Save(destPath);
                         }
@@ -111,7 +137,7 @@ namespace SSE
 
                         }
 
-                        string sessionName = "Maths Lecutre 2021.06.07";
+                        string sessionName = txtSessionName.Text;
 
                         startInfo.Arguments = $@"/k cd {path} && cd output && echo {sessionName} > hash.md5 && exit  ";
                         startInfo.CreateNoWindow = true;
@@ -122,12 +148,12 @@ namespace SSE
                         }
 
 
-                        string finalDestPath = $@"{path}\\{sessionName}.zip";
+                        string finalDestPath = $@"{outputPath}\\{sessionName}.zip";
 
                         using (ZipFile zip = new ZipFile())
                         {
 
-                            zip.Password = "test2";
+                            zip.Password = "pass2";
                             zip.AddDirectory(startPath);
                             zip.Save(finalDestPath);
                         }
@@ -140,12 +166,20 @@ namespace SSE
                             exeProcess.WaitForExit();
                         }
 
+                        lblStat.ForeColor = Color.Green;
+                        lblStat.Text = "Done";
+
+
 
                     }
                     catch (Exception fileEx)
                     {
+                        lblStat.ForeColor = Color.Red;
+                        lblStat.Text = "Error"; 
                         Console.WriteLine(fileEx.ToString());
                     }
+
+                    btnEncrypt.Enabled = true;
                 }
                 
 
@@ -155,9 +189,27 @@ namespace SSE
           
         }
 
-        private void button1_Click_2(object sender, EventArgs e)
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
+           
+        }
 
+        private void checkNvidia_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkLow.Checked)
+            {
+                checkLow.CheckState= CheckState.Unchecked;
+               
+            }
+        }
+
+        private void checkLow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkNvidia.Checked)
+            {
+                checkNvidia.CheckState= CheckState.Unchecked;
+                
+            }
         }
     }
 }
